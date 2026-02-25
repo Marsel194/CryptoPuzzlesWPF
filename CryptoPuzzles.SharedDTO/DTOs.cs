da5d8899fdb48ed4519b6ee22cc2e5c3cd627db9
Marsel194 <ajdjjddf@gmail.com>
2026-02-25 13:50:38 +0300

    Создание нового проекта с Dto

---
+++ CryptoPuzzles.SharedDTO/DTOs.cs
@@ -0,0 +1,12 @@
+﻿namespace CryptoPuzzles.SharedDTO
+{
+    public record AAdminDto(int Id, string Login, string FirstName, string LastName, string MiddleName, DateTime CreatedAt);
+    public record UAEncryptionMethod(int Id);
+    public record UAErrorResponse(string Message, string? Details);
+    public record UAGameSession(int Id);
+    public record UAHint(int Id);
+    public record UALoginResponse(int Id, string Login, string Email, string Username, string Token);
+    public record UAPuzzle(int Id);
+    public record UAUser(int Id, string Login, string Username, string Email, string CreatedAt);
+    public record UATutorial(int Id);
+}
