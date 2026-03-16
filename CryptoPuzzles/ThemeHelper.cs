5e5b17b8886a28bc63404eec639f85da5cd77bfa
Marsel194 <ajdjjddf@gmail.com>
2026-03-16 21:49:01 +0300

    Исправление цвета темы

--- CryptoPuzzles/ThemeHelper.cs
+++ CryptoPuzzles/ThemeHelper.cs
@@ -1,4 +1,6 @@
-﻿using MaterialDesignThemes.Wpf;
+﻿using System.Windows;
+using System.Windows.Media;
+using MaterialDesignThemes.Wpf;
 
 namespace CryptoPuzzles
 {
@@ -22,6 +24,8 @@ namespace CryptoPuzzles
 
             Properties.Settings.Default.Save();
             paletteHelper.SetTheme(theme);
+
+            UpdateMaterialDesignBody(theme.GetBaseTheme() == BaseTheme.Dark);
         }
 
         public static void ApplyTheme()
@@ -35,6 +39,19 @@ namespace CryptoPuzzles
                 theme.SetBaseTheme(BaseTheme.Light);
 
             paletteHelper.SetTheme(theme);
+            UpdateMaterialDesignBody(theme.GetBaseTheme() == BaseTheme.Dark);
+        }
+
+        private static void UpdateMaterialDesignBody(bool isDark)
+        {
+            Color targetColor = isDark
+                ? Color.FromRgb(0xDD, 0xDD, 0xDD)   // #FFDDDDDD
+                : Colors.Black;
+
+            var brush = new SolidColorBrush(targetColor);
+            brush.Freeze();
+
+            Application.Current.Resources["MaterialDesignBody"] = brush;
         }
     }
 }
